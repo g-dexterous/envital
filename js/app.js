c4d7bfe2c2d2d5f7b723c859547c4a3176495f2c
dexterous <dexterousgunua@gmail.com>
2013-07-21 19:10:54 +0530

    settings for username and api key plus some refactoring

--- js/app.js
+++ js/app.js
@@ -6,46 +6,49 @@ $.support.cors = true;
 
 
 var _previousBalance = 0,
-	_userName = "enter envato username here",
-	_apikey = "enter api here";
+	_userName = "",
+	_apikey = "";
+
+/*html(System.Gadget.Settings.read("userName"));*/
+randomNumber= function(){
+	//random number will be added to the request to prevent cached response
+	return (new Date()).getTime();
+};
 
 //System.Gadget.onSettingsClosed=SettingsClosed;
-(function getVitalData() {
-	
+function getVitalData() {
+	$.ajax({
+	  url: 'http://marketplace.envato.com/api/edge/'+_userName+'/'+_apikey+'/vitals.json?random='+randomNumber(),
+	  /*url: 'http://localhost/envatoAPI/vitals.json?random='+randomNumber(),*/
+	  type: 'GET',
+	  dataType: 'json',
+	  complete: function(xhr, textStatus) {
+	    setTimeout(getVitalData, 600000);
+	  },
+	  success: function(data, textStatus, xhr) {
+	    //called when successful
+	    var newBalance = Number(data.vitals.balance);
+	    if(_previousBalance!=0 && newBalance > _previousBalance){
+	    	//console.log("Got $$$");
+	    	System.Sound.playSound("../assets/sound/effect.wav");
+	    }
+	    _previousBalance = newBalance;
+	    var balancedisplay_str = newBalance.toFixed(2);
 
-	/*html(System.Gadget.Settings.read("userName"));*/
-	randomNumber= function(){
-		//random number will be added to the request to prevent cached response
-		return (new Date()).getTime();
-	};
-	  $.ajax({
-	      /*url: 'http://marketplace.envato.com/api/edge/'+_userName+'/'+_apikey+'/vitals.json?random='+randomNumber(),*/
-	      url: 'http://localhost/envatoAPI/vitals.json?random='+randomNumber(),
-		  type: 'GET',
-		  dataType: 'json',
-		  complete: function(xhr, textStatus) {
-		    setTimeout(getVitalData, 600000);
-		  },
-		  success: function(data, textStatus, xhr) {
-		    //called when successful
-		    var newBalance = Number(data.vitals.balance);
-		    if(_previousBalance!=0 && newBalance > _previousBalance){
-		    	//console.log("Got $$$");
-		    	System.Sound.playSound("../assets/sound/effect.wav");
-		    }
-		    _previousBalance = newBalance;
-		    var balancedisplay_str = newBalance.toFixed(2);
+	    $('#balance').html("$ "+balancedisplay_str);
+	  },
+	  error: function(xhr, textStatus, errorThrown) {
+	    //called when there is an error
+	    $("#balance").html(errorThrown);
+	  }
+	});
+};
 
-		    $('#balance').html("$ "+balancedisplay_str);
-		  },
-		  error: function(xhr, textStatus, errorThrown) {
-		    //called when there is an error
-		    $("#balance").html(errorThrown);
-		  }
-	  });
-})();
+function settingsHaveChanged(){
+	/*$("#balance").html("changed");*/
+	_userName = System.Gadget.Settings.read("userName");
+	_apikey = System.Gadget.Settings.read("apiKey");
 
-
-function onSettingsClosed(){
-
+	getVitalData();
+	$("#message").hide();
 }
--- js/settings.js
+++ js/settings.js
@@ -1,1 +1,25 @@
-System.Gadget.Settings.write("userName", "dexterous");
+/*
+$(document).ready(function() {
+	// Stuff to do as soon as the DOM is ready;
+	$("#saveBtn").click(function(event) {
+		// Act on the event
+		System.Gadget.Settings.write("userName", "dexterous");
+		$("#inUser").html("done");
+	});
+});*/
+
+System.Gadget.onSettingsClosing = function (event) {
+    // First, we need to make sure the user clicked the OK button to save
+    if (event.closeAction == event.Action.commit) {
+        // If they did, we need to store the settings
+        var username = document.getElementById("userid").value;
+        var apiKey = document.getElementById("apiKey").value;
+
+        System.Gadget.Settings.write("username", username);
+        System.Gadget.Settings.write("apiKey", apiKey);
+
+        /* Finally, we can call a function defined in the main gadget window's
+           JavaScript to let it know that the settings have changed */
+        System.Gadget.document.parentWindow.settingsHaveChanged();
+    }
+}
